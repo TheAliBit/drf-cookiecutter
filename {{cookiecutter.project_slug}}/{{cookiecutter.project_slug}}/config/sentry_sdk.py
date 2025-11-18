67ae71dd27ab170841d9638c5ee8b4ea76f61ade
TheAliBit <the.ali8bits@gmail.com>
2025-11-18 17:06:05 +0400

    fix: fix sentry config setting

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/sentry_sdk.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config/sentry_sdk.py
@@ -1,9 +1,15 @@
 import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 from decouple import config
 
-sentry_sdk.init(
-    dsn=config("SENTRY_DSN"),
-    integrations=[sentry_sdk.integrations.django.DjangoIntegration()],
-    traces_sample_rate=1.0,  # performance monitoring (0.0–1.0)
-    send_default_pii=True,  # optional – includes user info if logged in
-)
+SENTRY_DSN = config("SENTRY_DSN", default="")
+
+if SENTRY_DSN:  # Only initialize if DSN is provided
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+        send_default_pii=True,
+    )
+else:
+    print("Sentry DSN not configured. Skipping Sentry initialization.")
